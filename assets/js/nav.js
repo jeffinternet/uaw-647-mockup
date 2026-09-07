af8d6453df459cf3383b8cf6aaa80b1da97e57bf
jeffinternet <jalevline@gmail.com>
2026-09-07 17:04:34 -0400

    clear for v4

--- assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-function toggleDropdown(el){
-  var li = el.parentElement;
-  li.classList.toggle('open');
-}
-document.addEventListener('click', function(e){
-  document.querySelectorAll('.navlinks > li.has-dropdown.open').forEach(function(li){
-    if (!li.contains(e.target)){ li.classList.remove('open'); }
-  });
-});
